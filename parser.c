b5023ea8222d5a46d03fb082011c4623462eaeb8
Kevin Kerr <kevin@tl.local>
2014-10-16 10:02:21 -0400

    more lexing

--- parser.c
+++ parser.c
@@ -21,19 +21,41 @@ char *parseCmdLn(int argc, char ** argv, int *stat) {
 char *readFn(char* fname, int *stat) {
   const char * opentype  = "r";
   FILE *f = fopen(fname,openType);
+  if (f == NULL) {
+      *stat = -1;
+      return NULL;
+  }
   ssize_t n = BUFSIZE;
   ssize_t i = 0;
   char *fbuf = malloc(sizeof(char) * BUFSIZE);
   int status = fread(fbuf,BUFSIZE,1,f); 
   if (status == 0) {
-    &stat = 1;
+    *stat = 1;
   }
   return fbuf;
 };
 
 struct Token *allocTokenChunk(struct Token *base, int num) {
-  return realloc(base,num);
-}
+  return realloc(base,(num*sizeof(struct Token)));
+};
+
+int lexIdent(char **l) {
+    int n = 0;
+    while (('a' <= **l) && ('Z' >= **l)) {
+        n++;
+        *l++;
+    }
+    return n;
+};
+
+int parseInt(char **l) {
+    int n = 0;
+    while (('0' <= **l) &* ('9' >= **l)) {
+        n = ((n*10)+(**l-'0'));
+        *l++;
+    }
+    return n;
+};
 
 
 struct Token *lex(char *fbuf) {
@@ -47,14 +69,134 @@ struct Token *lex(char *fbuf) {
     int ii = tokenListSize;
     for (;i<tokenChunkSize;i++) {
       l = killwhite(l);
-      if (('a' <= *l) && ('Z' >= *l)) {
+      if (*l == '.') {
+          currentBase[i].type = DOT;
+          currentBase[i].value = ".";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == '[') {
+          currentBase[i].type = LBRAK;
+          currentBase[i].value = "[";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == ']') {
+          currentBase[i].type = RBRAK;
+          currentBase[i].value = "]";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == '(') {
+          currentBase[i].type = LPAREN;
+          currentBase[i].value = "(";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == ')') {
+          currentBase[i].type = RPAREN;
+          currentBase[i].value = ")";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == ')') {
+          currentBase[i].type = RPAREN;
+          currentBase[i].value = ")";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == '~') {
+          currentBase[i].type = TILDE;
+          currentBase[i].value = "~";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == '*') {
+          currentBase[i].type = STAR;
+          currentBase[i].value = "*";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (strcmp("DIV",l)) {
+          l+=3;
+          currentBase[i].type = DIV;
+          currentBase[i].value = "DIV";
+          currentBase[i].len = -1;
+      }
+      else if (strcmp("MOD",l)) {
+          l+=3;
+          currentBase[i].type = MOD;
+          currentBase[i].value = "MOD";
+          currentBase[i].len = -1;
+      }
+      else if (*l == '&') {
+          currentBase[i].type = AMPERSAND;
+          currentBase[i].value = "&";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == '+') {
+          currentBase[i].type = PLUS;
+          currentBase[i].value = "+";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == '-') {
+          currentBase[i].type = MINUS;
+          currentBase[i].value = "-";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (strcmp("OR",l)) {
+          currentBase[i].type = OR;
+          currentBase[i].value = "OR";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == '=') {
+          currentBase[i].type = EQUALS;
+          currentBase[i].value = "=";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == '#') {
+          currentBase[i].type = HASH;
+          currentBase[i].value = "#";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == '<') {
+          currentBase[i].type = LT;
+          currentBase[i].value = "<";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (*l == '>') {
+          currentBase[i].type = GT;
+          currentBase[i].value = ">";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (strcmp("<=",l)) {
+          currentBase[i].type = LTE;
+          currentBase[i].value = "<=";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (strcmp(">=",l)) {
+          currentBase[i].type = GTE;
+          currentBase[i].value = ">=";
+          currentBase[i].len = -1;
+          l++;
+      }
+      else if (('a' <= *l) && ('Z' >= *l)) {
         currentBase[i].type = IDENT;
         currentBase[i].value = l;
-        currentBase[i].len = lexIdent(l);
+        currentBase[i].len = lexIdent(&l);
       }
-      if (('0' <= *l) &* ('9' >= *l)) {
+      else if (('0' <= *l) &* ('9' >= *l)) {
         currentBase[i].type = INTEGER;
-        currentBase[i].value = parseInt(l);
+        currentBase[i].value = parseInt(&l);
         currentBase[i].len = -1;
       }
     }
