a7fb691d0d1e147d94a4e6619c34a5c7a9843b9c
Kevin Kerr <kevin@tl.local>
2014-10-19 23:52:05 -0400

    making progress

--- eval.js
+++ eval.js
@@ -40,7 +40,7 @@ var Token = function (terminal,match) {
 
 var oberonTerminals = [];
 
-oberonTerminals.push(new Terminal("ASTERIX",/^\*/));
+oberonTerminals.push(new Terminal("ASTER",/^\*/));
 oberonTerminals.push(new Terminal("DIV",/^DIV/));
 oberonTerminals.push(new Terminal("MOD",/^MOD/));
 oberonTerminals.push(new Terminal("AND",/^&/));
@@ -56,6 +56,7 @@ oberonTerminals.push(new Terminal("GTE",/^>=/));
 oberonTerminals.push(new Terminal("DOT",/^\./));
 oberonTerminals.push(new Terminal("COMMA",/^,/));
 oberonTerminals.push(new Terminal("COLON",/^:/));
+oberonTerminals.push(new Terminal("SEMI",/^;/));
 oberonTerminals.push(new Terminal("RPAREN",/^\)/));
 oberonTerminals.push(new Terminal("LPAREN",/^\(/));
 oberonTerminals.push(new Terminal("RBRAK",/^\]/));
@@ -83,33 +84,158 @@ oberonScanner = new Scanner(oberonTerminals);
 var OberonEvaluator = function (oberonScanner) {
     this.eval = function (input,env) {
         var tokens = oberonScanner.scan(input);
+        return evalModule(tokens);
+    };
+    var EnvRecord = function (type,value) {
+        this.type = type;
+        this.value = value;
+    };
+    var error = function (type,token) {
+        throw new Error(type + token);
+    }
+    var evalModule = function (tokens) {
         var lead = tokens[i];
-        switch (lead.type) {
-            case "INT" :
-                return parseInt(lead.value);
-                break;
-            case "IDENT" : 
-                return lookup(lead.value,env);
-                break;
-            case "MODULE":
-                return makeModule(input.slice(1),env);
-                break;
-            case "PROCEDURE":
-                return makeProcedure(input.slice(1),env);
-                break;
-            case "VAR":
-                return makeVar(input.slice(1),env);
-                break;
-            case "TYPE":
-                return makeType(input.slice(1),env);
-                break;
-            case "CONST":
-                return makeConst(input.slice(1),env);
-                break;
-            case "RECORD":
-                return makeRecord(input.slice(1),env);
-                break;
-            case "ARRAY":
-                return makeArray(input.slice(1),env);
-            case 
+        var rest = tokens.slice(1);
+        var results;
+        if (lead.type != 'MODULE') {
+            error("expected token Module", lead);
+        }
+        var env = {};
+        if (rest[0].type != 'IDENT') {
+            error("expected token Ident",rest[0]);
+        }
+        var moduleName = rest[0].value;
+        rest = rest.slice(1);
+        env[moduleName] = new EnvRecord("MODULE",{});
+        if (rest[0].type != "SEMI") {
+            error("expected token Semi", rest[0]);
+        }
+        rest = rest.slice(1);
+        rest = evalDecls(rest,env[moduleName].value);
+        if (rest[0].type == 'BEGIN') {
+            rest = evalStatementSeq(rest.slice(1),env,results);
+        }
+        if (rest[0].type != 'END') {
+            error("expected token END", rest[0]);
+        }
+        rest = rest.slice(1);
+        if (rest[0].type != 'IDENT' && rest[0].value != moduleName) {
+            error("bad end");
+        }
+        rest = rest.slice(1);
+        if (rest[0].type != 'DOT') {
+            error("expected dot",rest[0]);
+        }
+        rest = rest.slice(1);
+        if (rest.length) {
+            error("too many tokens");
+        }
+        return results;
+    };
+    var evalDecls = function (tokens, modEnv) {
+        var lead = tokens[0];
+        var rest = tokens.slice(1);
+        var progress = function () {
+            lead = rest[0];
+            rest = rest.slice(1);
+        }
+        if (lead.type == 'CONST') {
+            rest = makeConst(rest,modEnv);
+            progress();
+        }
+        if (lead.type == 'TYPE') {
+            rest = makeType(rest,modEnv);
+            progress();
+        }
+        if (lead.type == 'VAR') {
+            rest = makeVar(rest,modEnv);
+            progress();
+        }
+        while (lead.type == 'PROCEDURE') {
+            rest = makeProcedure(rest,modEnv);
+            if (rest[0].type != "SEMI") {
+                error("expected semi",rest[0]);
+            }
+            progress();
+            progress();
+        }
+        return rest;
+    };
+    var evalStatementSeq = function (tokens,env,results) {
+        var lead = tokens[0];
+        var rest = tokens.slice(1);
+        var progress = function () {
+            lead = rest[0];
+            rest = rest.slice(1);
+        }
+        if (lead.type == 'IDENT') {
+            var ident = lookUp(lead.value,env);
+            if (ident.type == 'PROCEDURE') {
+                rest = invokeProcedure(ident,rest,env,results);
+            }
+            else {
+                evalAssignments(ident,rest,env,results);
+            }
+        }
+        else if (lead.type == 'IF') {
+            var bool = [];
+            rest = evalExpr(rest,env,bool);
+            if (bool[0]) {
+                progress();
+                if (lead.type != "THEN") {
+                    error("expected THEN",lead);
+                }
+                rest = evalStatementSeq(rest,env,results);
+            }
+            else {
+                progress();
+                if (lead.type != "THEN") {
+                    error("expected THEN",lead);
+                }
+                rest = evalStatementSeq(rest,env,[]); // just to push the evaluator to the right token
+                progress();
+                var test = false;
+                while (lead.type == 'ELSIF') {
+                    bool = [];
+                    rest = evalExpr(rest,env,bool);
+                    if (bool[0]) {
+                        progress();
+                        if (lead.type != "THEN") {
+                            error("expected THEN",lead);
+                        }
+                        rest = evalStatementSeq(rest,env,results);
+                        progress();
+                        test = true;
+                        break;
+                    }
+                    progress();
+                    if (lead.type != "THEN") {
+                        error("expected THEN",lead);
+                    }
+                    rest = evalStatementSeq(rest,env,[]);
+                    progress();
+                }
+                if (!test) {
+                    if (lead.type == 'ELSE') {
+                        rest = evalStatmentSeq(rest,env,results);
+                    }
+                }
+                if (lead.type == 'ELSE') {
+                    rest = evalStatmentSeq(rest,env,results);
+                }
 
+
+
+
+
+
+
+        
+            
+
+        
+        
+        
+                         
+
+                 
